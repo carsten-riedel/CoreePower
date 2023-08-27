b59f4c63b0004f19c20b5122cdbdfb67a6ee8b03
Carsten Riedel <carstenriedel@outlook.com>
2023-08-27 13:20:20 +0200

    removes test project

--- CoreePower.Foo/src/CoreePower.Foo.ps1
+++
@@ -1,8 +0,0 @@
-
-function SampleFunction {
-    [Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSUseApprovedVerbs", "")]
-    [alias("sf")]
-    param()
-    Write-Output "Hello World!"
-}
-
--- CoreePower.Foo/test/RunnerTests.ps1
+++
@@ -1,7 +0,0 @@
-    function Test-SampleFunction {
-        param()
-        [bool]$retval = $false;
-        SampleFunction
-        [bool]$retval = $true
-        return $retval
-    }
