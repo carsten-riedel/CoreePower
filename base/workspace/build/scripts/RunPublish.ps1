407dddf467b0f32364add95ca40d5621812e85ad
Carsten Riedel <carstenriedel@outlook.com>
2023-09-17 22:42:42 +0200

    update targets and publish

--- base/workspace/build/scripts/RunPublish.ps1
+++ base/workspace/build/scripts/RunPublish.ps1
@@ -2,7 +2,7 @@ $dir = $PSScriptRoot
 Set-Location -Path "$dir/../.."
 $workspaceFolder = Get-Location
 
-$dotnetdir = Join-Path -Path "$workspaceFolder" -ChildPath "source/CoreePower.Net/src/CoreePower.Net"
+$dotnetdir = Join-Path -Path "$workspaceFolder" -ChildPath "source" -AdditionalChildPath @("CoreePower.Net","src","CoreePower.Net")
 $artifacts = Join-Path -Path "$workspaceFolder" -ChildPath "artifacts" -AdditionalChildPath @("bin")
 
 Set-Location -Path $dotnetdir
@@ -10,16 +10,17 @@ Set-Location -Path $dotnetdir
 #dotnet clean --configuration "Release" --framework "net461" --property:TargetFrameworks="net461" & dotnet publish --configuration "Release" --framework "net461" --property:TargetFrameworks="net461" --property:DefineConstants="net461" --output "bin\Publish\net461" --force
 
 &dotnet clean `
-  --configuration:"Debug" `
+  --configuration:"Release" `
   --framework:"netstandard2.0" `
   --property:"TargetFrameworks=`"netstandard2.0`""
 
 &dotnet publish `
   --force `
-  --configuration:"Debug" `
+  --configuration:"Release" `
   --framework:"netstandard2.0" `
   --property:"TargetFrameworks=`"netstandard2.0`"" `
-  --output:"$artifacts" 
+  --output:"$artifacts" `
+  --maxcpucount:1
   
 
 Set-Location -Path $workspaceFolder
